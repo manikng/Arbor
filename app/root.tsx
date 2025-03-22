8cb2eed342cbaa7c22eb4b54c014ba380ea53954
manikng <manishsingh_23cs244@dtu.ac.in>
2025-03-22 14:17:42 +0530

    add Cache-Control headers and remove sensitive environment variables from Vercel configuration

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,13 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+
+export function headers(_: Route.HeadersArgs) {
+  return {
+    'Cache-Control': 's-maxage=1, stale-while-revalidate=59',
+  };
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
